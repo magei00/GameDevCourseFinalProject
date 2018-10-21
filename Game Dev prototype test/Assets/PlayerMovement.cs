4059c5f8e2468febdf62e619c4500c4ef182ab6b
magei00 <magnuseilersen@yahoo.dk>
2018-10-21 23:52:25 +0200

    Created Player

---
+++ Game Dev prototype test/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+	}
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position+moveVelocity*Time.fixedDeltaTime);
+    }
+}
