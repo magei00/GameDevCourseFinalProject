9aa1ca6bf220768fd5af62c511b7b5fa22c72aeb
unknown <mattik15@gmail.com>
2018-11-05 17:57:31 +0100

    First level made

---
+++ Adam Game Mechanics Prototype/Assets/KeyCollect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCollect : MonoBehaviour {
+
+    public GameObject doorToOpen;
+    Animator doorAnimator;
+
+
+    void Awake()
+    {
+        doorAnimator = doorToOpen.GetComponent<Animator>();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Entered the trigger");
+        if (other.CompareTag("Player"))
+        {
+            doorAnimator.SetBool("key_collected", true);
+            Debug.Log("Player enter");
+            OpenDoor();
+            Destroy(gameObject);
+        }
+    }
+
+    void OpenDoor()
+    {
+        BoxCollider2D boxCollider = doorToOpen.GetComponent<BoxCollider2D>();
+        boxCollider.enabled = true;
+    }
+}
