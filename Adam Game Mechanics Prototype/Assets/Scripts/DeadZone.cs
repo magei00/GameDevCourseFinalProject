d11d8884b8398f2ec99e85fa5080c6b32893db9e
magei00 <magnuseilersen@yahoo.dk>
2018-11-06 13:19:26 +0100

    Added DeadZone that restarts the level if Player collides with it.

---
+++ Adam Game Mechanics Prototype/Assets/Scripts/DeadZone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeadZone : MonoBehaviour {
+
+    private SpriteRenderer spriteRenderer; 
+
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
