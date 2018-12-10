3db22cd60b26d84aed6f3709dea1793ed85c8537
unknown <mattik15@gmail.com>
2018-12-10 17:59:14 +0100

    Spikes and level design

---
+++ Adam Game Mechanics Prototype/Assets/Spikes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Spikes : MonoBehaviour
+{
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerPlatformerController>().Kill();
+
+        }
+    }
+}
